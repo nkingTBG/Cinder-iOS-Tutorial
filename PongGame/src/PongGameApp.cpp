cf35c6c3c2002e58263102ea87a52a4026aa55f4
Noah King <nking@barbariangroup.com>
2011-02-09 12:12:53 -0500

    push to device working, screensize fixed, paddle moves with touch

--- PongGame/src/PongGameApp.cpp
+++ PongGame/src/PongGameApp.cpp
@@ -1,12 +1,18 @@
 #include "cinder/app/AppCocoaTouch.h"
+#include "cinder/app/App.h"
 #include "cinder/app/Renderer.h"
 #include "cinder/Surface.h"
 #include "cinder/gl/Texture.h"
 #include "cinder/Camera.h"
 #include "cinder/Rand.h"
+#include <iostream>
+#include <sstream>
+#include <fstream>
 
 using namespace ci;
 using namespace ci::app;
+using namespace std;
+using namespace gl;
 
 class PongGame : public AppCocoaTouch {
 public:
@@ -15,30 +21,41 @@ public:
 	virtual void	update();
 	virtual void	draw();
 	virtual void	mouseDown( MouseEvent event );
+	virtual void	mouseDrag( MouseEvent event );
 	
-	CameraPersp	mCam;
 	Vec2f pos, vel, acc;
 	float wid, hei, rad;
+	Vec2f paddleCenter;
+	float paddleWidth, paddleHeight;
 };
 
 void PongGame::setup()
 {
 	pos = Vec2f(0,0);
-	vel = Vec2f( Rand::randFloat(6),Rand::randFloat(6) );
+	vel = Vec2f( Rand::randFloat(2),Rand::randFloat(2) );
 	acc = Vec2f(0,0);
-	wid = getWindowWidth() * 0.5f;
-	hei = getWindowHeight() * 0.5f;
+	wid = getWindowWidth();
+	hei = getWindowHeight();
 	rad = 25.0f;
+	paddleHeight = 10.0f;
+	paddleCenter = Vec2f(getWindowCenter().x,getWindowHeight() - paddleHeight * 2);
+	paddleWidth = 80.0f;
 }
 
 void PongGame::resize( ResizeEvent event )
 {
-	mCam.lookAt( Vec3f( 0, 0, -hei * 2.2f ), Vec3f::zero(), Vec3f(0,-1,0) );
-	mCam.setPerspective( 60, event.getAspectRatio(), 1, 2000 );
+	//mCam.lookAt( Vec3f( 0, 0, -hei * 2.2f ), Vec3f::zero(), Vec3f(0,-1,0) );
+	//mCam.setPerspective( 60, event.getAspectRatio(), 1, 2000 );
 }
 
 void PongGame::mouseDown( MouseEvent event )
 {
+	paddleCenter.x = event.getPos().x;
+}
+
+void PongGame::mouseDrag( MouseEvent event )
+{
+	paddleCenter.x = event.getPos().x;
 }
 
 void PongGame::update()
@@ -46,17 +63,17 @@ void PongGame::update()
 	vel += acc;
 	pos += vel;
 	
-	if(pos.x < -wid + rad){
+	if(pos.x < 0 + rad){
 		vel.x *= -1.0f;
-		pos.x = -wid + rad;
+		pos.x = 0 + rad;
 	} else if(pos.x > wid - rad){
 		vel.x *= -1.0f;
 		pos.x = wid - rad;
 	}
 	
-	if(pos.y < -hei + rad){
+	if(pos.y < 0 + rad){
 		vel.y *= -1.0f;
-		pos.y = -hei + rad;
+		pos.y = 0 + rad;
 	} else if(pos.y > hei - rad){
 		vel.y *= -1.0f;
 		pos.y = hei - rad;
@@ -65,16 +82,27 @@ void PongGame::update()
 
 void PongGame::draw()
 {
-	gl::clear( Color( 1.0f, 0.5f, 0.15f ) );
+	clear();
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	
-	gl::setMatrices( mCam );
-	gl::color( ColorAf(0,0,0,0.85f) );
-	gl::drawSolidRect( Rectf(-wid, -hei, wid, hei) );
-	gl::color(Colorf(1,1,1));
-	gl::drawStringCentered("Hello World", Vec2f(0, hei));
-	gl::drawSolidCircle(pos, rad ,32);
+	setMatricesWindow( getWindowSize() );
+	color( ColorAf(0,0,0,0.85f) );
+	drawSolidRect( Rectf( 0, 0, wid, hei) );
+	color(Colorf(1,1,1));
+	
+	
+	string line = "test";
+	
+	drawStringCentered( line, getWindowCenter(), Colorf(1,1,1), Font( "Gill Sans", 60 ) );
+	cout << getWindowCenter();
+	//gl::drawStringCentered("Hello World", Vec2f(0, hei));
+	color(Colorf(1,0,0));
+	drawSolidCircle(pos, rad ,32);
+	
+	color(Colorf(1,1,1));
+	
+	drawSolidRect( Rectf(paddleCenter.x - paddleWidth, paddleCenter.y - paddleHeight, paddleCenter.x + paddleWidth, paddleCenter.y + paddleHeight ) );
 	
 	
 }
